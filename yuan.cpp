f01d5874f1a3a1bdd37533a3a0767f2e21edc4c2
Richard <1056144048@qq.com>
2021-11-03 22:51:28 +0800

    master

---
+++ yuan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double r,l,s;           //r,l,s分别表示圆的半径、周长、面积
+    r = 2.0;
+    l=2.0*3.14*r;
+    s = 2.0 *3.14*r*r;
+    cout<<"l="<<l;
+    cout<<"   s="<<s<<endl;
+}
